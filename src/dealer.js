121eeb5510c9a4c0d1ec372f8607bc791e031e82
Adda Skogberg <adda.skogberg@gmail.com>
2017-10-18 21:07:06 +0200

    commenting dealer

--- src/dealer.js
+++ src/dealer.js
@@ -7,16 +7,26 @@
 
 'use strict'
 
+// collects the functions from player enabeling an instance of player in dealer.
 const Player = require('./player')
 
 class Dealer {
+  /**
+   * constructor for a dealer who is a player
+   * @param  {deck} - dealers deck of cards.
+   */
   constructor (deck) {
     this.deck = deck
     this.player = new Player()
   }
+  /**
+   * function providing a card from the deck in use.
+   * @return {card} - returns a card from the deck
+   */
   askForNewCard () {
     return this.deck.getCard()
   }
 }
 
+// exports dealer.
 module.exports = Dealer
