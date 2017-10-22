5ad255042080ef8cdc62b94ecbeb87849107e07f
Adda Skogberg <adda.skogberg@gmail.com>
2017-10-22 18:46:47 +0200

    commenting and adding to the function prepareNewRound

--- app.js
+++ app.js
@@ -5,23 +5,27 @@
  */
 
 'use strict'
-
+// welcome frase
 console.log('\u2665 \u2666 \u2663 \u2660 \u2665 \u2666 \u2663 \u2660 \u2665 \u2666 \u2663 \u2660 \u2665 \u2666 \u2663 \u2660')
 console.log('     Welcome to Blackjack')
 console.log('\u2665 \u2666 \u2663 \u2660 \u2665 \u2666 \u2663 \u2660 \u2665 \u2666 \u2663 \u2660 \u2665 \u2666 \u2663 \u2660')
 console.log()
 
+// makes deck, dealer and player available
 const Deck = require('./src/deck.js')
 const Dealer = require('./src/dealer.js')
 const Player = require('./src/player.js')
 
+// instanciates the deck adds the cards and shuffles the deck
 let myDeck = new Deck()
 myDeck.addCards()
 myDeck.shuffleDeck()
 
+// instanciates the player and dealer(a type of player)
 let player = new Player()
 let dealer = new Dealer(myDeck)
 
+// The game logic and print to console
 for (let i = 0; i < 20; i++) {
   while (player.getSum() < 15) {
     player.addCard(dealer.askForNewCard())
@@ -60,8 +64,17 @@ for (let i = 0; i < 20; i++) {
   PrepareNewRound()
 }
 
+/**
+ * prepares a new round by emptying the hands and moving the used cards
+ * to the trash deck
+ */
+
 function PrepareNewRound () {
   let cards = player.emptyHand()
+  for (let i = 0; i < cards.length; i++) {
+    myDeck.addToTrash(cards[i])
+  }
+  cards = dealer.player.emptyHand()
   for (let i = 0; i < cards.length; i++) {
     myDeck.addToTrash(cards[i])
   }
